61f87acaa3a964146c4e4ab3fc0a6949119d0901
crystalchavez99 <chavezcrystal1999@yahoo.com>
2024-01-05 08:45:41 -0800

    add user to the backend

--- backend/Learning.API/Models/Course.cs
+++ backend/Learning.API/Models/Course.cs
@@ -1,10 +1,15 @@
-﻿namespace Learning.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Learning.API.Models
 {
     public class Course
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Level { get; set; }
+        [Required]
         public string SchoolYear { get; set; }
     }
 }
