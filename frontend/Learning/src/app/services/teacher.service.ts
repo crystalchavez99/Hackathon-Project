761ea2d7eefcec2c8fbd440c4b43ce18eebda05e
crystalchavez99 <chavezcrystal1999@yahoo.com>
2024-01-05 14:43:19 -0800

    teacher service made implmeneted

--- frontend/Learning/src/app/services/teacher.service.ts
+++ frontend/Learning/src/app/services/teacher.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Teacher } from '../models/teacher';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TeacherService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  registerTeacher(model: Teacher): Observable<Teacher>{
+    return this.http.get<Teacher>(`https://localhost:7232/api/teachers/register`);
+  }
+
+  loginTeacher(model: Teacher): Observable<Teacher>{
+    return this.http.get<Teacher>(`https://localhost:7232/api/teachers/login`);
+  }
 }
